c3e723794fcb1ff63ac253e934ff15b634173b94
irtaimal <ir.taimal10@uniandes.edu.co>
2020-05-28 00:15:31 -0500

    Create favor controller

--- src/partner-platform/config/serviceAccountKey.ts
+++ src/partner-platform/config/serviceAccountKey.ts
@@ -1,8 +1,10 @@
+
+const private_key = process.env.partner_platform_notification__private_key;
 export const configuration: any = {
     type: process.env.partner_platform_notification__type,
     project_id: process.env.partner_platform_notification__project_id,
     private_key_id: process.env.partner_platform_notification__private_key_id,
-    private_key: process.env.partner_platform_notification__private_key,
+    private_key: private_key.replace(/\\n/g, '\n'),
     client_email: process.env.partner_platform_notification__client_email,
     client_id: process.env.partner_platform_notification__client_id,
     auth_uri: process.env.partner_platform_notification__auth_uri,
